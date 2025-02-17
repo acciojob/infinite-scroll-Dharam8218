c31f3387c663692748c1e23a30f28c2a1c1d983e
Dharam8218 <101041058+Dharam8218@users.noreply.github.com>
2025-02-17 12:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
 
+const mainList = document.getElementById("infi-list");
+function addListItems(count) {
+    for (let i = 0; i < count; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `Item ${mainList.children.length + 1}`;
+        mainList.appendChild(listItem);
+    }
+}
+addListItems(10);
+mainList.addEventListener("scroll", () => {
+	if (mainList.scrollTop + mainList.clientHeight > mainList.scrollHeight-1) {
+        // Add 2 more list items when the end is reached
+		alert("Reached bottom");
+        addListItems(2);
+    }
+});
+
+
+
+
+
+
+
+
+
